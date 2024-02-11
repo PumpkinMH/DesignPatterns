7b31685170f80ff63e2a92270fa4f361946d14de
Mark Hurd <mahurd@email.sc.edu>
2024-02-11 16:12:35 -0500

    Added Javadoc comments

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -4,17 +4,39 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class FlashCards {
+  /**
+   * Random variable used for picking the next word
+   */
   private Random rand;
+
+  /**
+   * Static member used for a singleton
+   */
   private static FlashCards flashCards;
-  private ArrayList<Word> words;  
+
+  /**
+   * Represents all of the words from the word file
+   */
+  private ArrayList<Word> words;
+
+  /**
+   * The current word that will be used in getWord
+   */
   private Word currentWord;
 
+  /**
+   * Private constructor for FlashCards available via getInstance
+   */
   private FlashCards() {
     rand = new Random();
     words = FileReader.getWords();
     currentWord = words.get(rand.nextInt(words.size()));
   }
 
+  /**
+   * Returns a the same instance of FlashCards each time. If it doesn't exist, it will make it exactly once
+   * @return Singleton FlashCard object 
+   */
   public static FlashCards getInstance() {
     if(flashCards == null) {
       flashCards = new FlashCards();
@@ -22,6 +44,10 @@ public class FlashCards {
     return flashCards;
   }
 
+  /**
+   * Returns the current Word that was randomly selected
+   * @return Word representing the current Word
+   */
   public Word getWord() {
     currentWord = words.get(rand.nextInt(words.size()));
     return currentWord;
--- singleton/Word.java
+++ singleton/Word.java
@@ -1,8 +1,18 @@
 package singleton;
 
 public class Word {
+  /**
+   * Strings representing the word, part of speech, definition, and an example sentence
+   */
   private String word, type, definition, sentence;
   
+  /**
+   * Constructor that initializes all of the members
+   * @param word String representing the word
+   * @param type String representing the part of speech
+   * @param definition String representing the definition
+   * @param sentence String representing the example sentence
+   */
   public Word(String word, String type, String definition, String sentence) {
     this.word = word;
     this.type = type;
@@ -10,10 +20,18 @@ public class Word {
     this.sentence = sentence;
   }
 
+  /**
+   * Returns a String representation of the word
+   * @return String representation of the word
+   */
   public String getWord() {
     return word;
   }
 
+  /**
+   * Returns a String representation of the part of speech, definition, and the sentence.
+   * @return String representation of the Word excluding the actual word itself
+   */
   public String getDescription() {
     String temp = "";
     temp += "Part of Speech: ";
